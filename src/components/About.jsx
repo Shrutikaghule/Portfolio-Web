d3254b63637391dc65d916f8340b494f932116fe
shrutika <ghuleshrutika20@gmail.com>
2024-03-29 01:30:27 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
             <section className=" px-20 dark:bg-white  bg-[#000814]">
                 <h2 className="text-4xl font-bold text-white ">About Me</h2>
                 <p className="text-xl text-[#edf6f9] py-6 px-10">
-                    I am a software developer with a passion for web development. I am currently working as a software developer at a startup. I have experience in building web applications using React, Node.js, and MongoDB. I am also familiar with AWS and Azure. I am always eager to learn new technologies and improve my skills. I am passionate about building products that are user-friendly and accessible.
+                I’m a tech enthusiast currently pursuing my bachelor’s degree. An organized and motivated individual capable of time management and working under pressure in all environments. Seeking an internship to use my skills against Real Time challenges.
                 </p>
             </section>
         </div>
